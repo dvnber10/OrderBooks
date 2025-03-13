1d445aeb797a5a125b3e28581dfc28a4059a0825
dvnber10 <dvnber2001@gmail.com>
2025-03-13 08:44:32 -0500

    <feat> Second commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.controllers import user_controller
+from app.controller import user_controller
 from app.database import engine, Base
 
 Base.metadata.create_all(bind=engine)  # Crea las tablas si no existen
